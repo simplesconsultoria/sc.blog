f48903b158ec191630d89f0230996e0f3e0b6e3b
hvelarde <hector.velarde@gmail.com>
2013-03-15 19:23:41 -0300

    Preparing release 1.0a1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '1.0a2.dev0'
+version = '1.0a1'
 long_description = open("README.rst").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
